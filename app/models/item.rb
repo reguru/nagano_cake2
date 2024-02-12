2f66b4ef249bb144dcaad106e24e4f7a73bc2e0c
EC2 Default User <ec2-user@ip-172-31-21-65.ap-northeast-1.compute.internal>
2024-02-12 18:16:20 +0000

    modelアソシエーション完了

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,17 @@
 class Item < ApplicationRecord
+
+  has_many :cart_items, dependent: :destroy
+  belongs_to :genre
+  has_many :order_details, dependent: :destroy
+
+  has_one_attached :image
+
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+      image
+  end
+
 end
