10c497dac98ac669225a2d3f1a9d4bfe9d6211bd
EC2 Default User <ec2-user@ip-172-31-21-65.ap-northeast-1.compute.internal>
2024-02-27 17:43:52 +0000

    注文情報入力画面作成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -10,19 +10,39 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
-    
+
   end
 
-  def comfirm
+  def confirm
+    @order = Order.new(order_params)
+    if params[:order][:address_number] == "1"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.last_name + current_customer.first_name
+
+    elsif params[:order][:address_number] == "2"
+      @address = current_customer.addresses.find(params[:order][:address_number_2])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
+
+    elsif params[:order][:address_number] == "3"
+      @order.postal_code = params[:order][:postal_code]
+      @order.address = params[:order][:address]
+      @order.name = params[:order][:name]
+
+    else
+      render :new
+    end
 
   end
 
   def complete
 
   end
-  
+
   private
-  
+
   def order_params
     params.require(:order).permit(:address, :name, :shipping_cost, :total_payment, :payment_method, :status)
   end
