17ac3a9403f1ea1cd45a833ed4d8a3a12c1f848c
EC2 Default User <liluru554@gmail.com>
2024-03-15 18:33:42 +0000

    退会画面作成

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,12 +1,17 @@
 class Public::AddressesController < ApplicationController
   def index
+    @addresses = current_customer.address.all
+    @address = Address.new
   end
 
   def edit
   end
 
   def create
-
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    @address.save
+    redirect_to addresses_path
   end
 
   def update
@@ -15,5 +20,11 @@ class Public::AddressesController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:name, :postal_code, :address)
   end
 end
