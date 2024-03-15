671ff155a702c2ba7f4c2a4564b0382881a8ccac
EC2 Default User <liluru554@gmail.com>
2024-03-15 19:14:37 +0000

    配送先画面作成

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,25 +1,37 @@
 class Public::AddressesController < ApplicationController
   def index
-    @addresses = current_customer.address.all
     @address = Address.new
+    @addresses = current_customer.addresses
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def create
     @address = Address.new(address_params)
     @address.customer_id = current_customer.id
-    @address.save
-    redirect_to addresses_path
+    if @address.save
+      redirect_to addresses_path
+    else
+      @addresses = current_customer.addresses
+      render :index
+    end
   end
 
   def update
-
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      redirect_to addresses_path
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @address = Address.find(params[:id])
+    @address.destroy
+    redirect_to addresses_path
   end
 
   private
