15b5757766b2236da66a73bf80088052db8e6f04
EC2 Default User <liluru554@gmail.com>
2024-03-19 17:58:19 +0000

    全機能完成

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -2,11 +2,18 @@ class Admin::OrderDetailsController < ApplicationController
   before_action :authenticate_admin!
 
   def update
-    @order = Order.find(params[:id])
     @order_detail = OrderDetail.find(params[:id])
+    @order = @order_detail.order
+    order_details = @order.order_details
     @order_detail.update(order_detail_params)
+    if order_details.where(making_status: 2).count >= 1
+      @order.update(status: 2)
+    elsif order_details.count == order_details.where(making_status: 3).count
+      @order.update(status: 3)
+    end
     redirect_to admin_order_path(@order)
   end
+
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     resources :genres, only: [:index, :create, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
     resources :orders, only: [:show, :update]
-    patch 'order_details/:id' => 'order_details#update'
+    resources :order_details, only: [:update]
   end
 
   scope module: :public do
