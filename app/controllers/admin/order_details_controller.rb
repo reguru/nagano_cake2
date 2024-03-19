00d7d02d77a5425f68f235375fd96281c8f843a6
EC2 Default User <liluru554@gmail.com>
2024-03-19 08:53:18 +0000

    注文履歴詳細完成

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -2,6 +2,15 @@ class Admin::OrderDetailsController < ApplicationController
   before_action :authenticate_admin!
 
   def update
+    @order = Order.find(params[:id])
+    @order_detail = OrderDetail.find(params[:id])
+    @order_detail.update(order_detail_params)
+    redirect_to admin_order_path(@order)
+  end
 
+  private
+
+  def order_detail_params
+    params.require(:order_detail).permit(:making_status)
   end
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,10 +1,24 @@
 class Admin::OrdersController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def show
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
   end
 
   def update
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
+    @order.update(order_params)
+    if @order.status == "入金確認"
+      @order_details.update_all(making_status: 1)
+    end
+    redirect_to admin_order_path(@order)
+  end
 
+  private
+
+  def order_params
+    params.require(:order).permit(:status)
   end
 end
